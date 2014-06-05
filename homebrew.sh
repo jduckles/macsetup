dd07d0f633d90b231a50eede9c77f872f83410fb
Jonah Duckles <jduckles@gmail.com>
2014-06-05 14:13:38 -0500

    Base install of apps needed for my mac

---
+++ homebrew.sh
@@ -0,0 +1,29 @@
+#!/bin/sh 
+## Install homebrew if necessary and install source and cask packages
+
+APPSLIST=https://github.com/jduckles/macconfig/apps/raw
+BREWLIST=https://github.com/jduckles/macconfig/brews/raw
+
+installbrew() {
+    hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"}
+
+}
+
+brews() {
+    for app in $(curl ${BREWLIST}); do
+        brew install $app
+    done
+}
+
+casks() {
+    for app in $(curl ${APPSLIST}); do
+        brew cask install $app
+    done
+}
+
+installbrew
+brews
+casks
+
+
+
