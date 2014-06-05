128bdb16551ed7cd9abfdf7c4d6496aa74bbff40
Jonah Duckles <jonahd@gmail.com>
2014-06-05 14:49:42 -0500

    Comments

--- homebrew.sh
+++ homebrew.sh
@@ -1,22 +1,27 @@
 #!/bin/sh 
 ## Install homebrew if necessary and install source and cask packages
-
-APPSLIST=https://raw.githubusercontent.com/jduckles/macconfig/master/apps
-BREWLIST=https://raw.githubusercontent.com/jduckles/macconfig/master/brews
+GITUSER=jduckles
+APPSLIST=https://raw.githubusercontent.com/${GITUSER}/macconfig/master/apps
+BREWLIST=https://raw.githubusercontent.com/${GITUSER}/macconfig/master/brews
 
 installbrew() {
-    hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)";}
-    brew tap caskroom/cask
-    brew install caskroom/cask/brew-cask
+    # If we can't find homebrew, install it
+    hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)";
+        brew tap caskroom/cask;
+        brew install caskroom/cask/brew-cask;
+    }
 }
 
+
 brews() {
+    # Install all brews 
     for app in $(curl ${BREWLIST}); do
         brew install $app
     done
 }
 
 casks() {
+    # install all casks in apps
     for app in $(curl ${APPSLIST}); do
         brew cask install $app
     done
